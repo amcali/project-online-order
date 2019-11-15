de4f2170c001ba450a2afe01de02ed2f908b90d5
Ubuntu <ubuntu@ip-172-31-39-221.ec2.internal>
2019-11-15 02:38:38 +0000

    Created login page, and linked the respective nav link to it.

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import auth, messages
 
 # Create your views here.
+
+""" This is the landing page for user account """
 def index(request):
     return render(request, "accounts/index.template.html")
 
+""" This is the logout function """
 def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully logged out")
     return redirect(index)
+    
+""" This is the user login function """
+def login(request):
+    return render(request, 'accounts/login.template.html')
