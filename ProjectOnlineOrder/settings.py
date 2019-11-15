858c073de9d54dd682ae042ffb8848c14f1d10ec
Ubuntu <ubuntu@ip-172-31-39-221.ec2.internal>
2019-11-15 07:11:53 +0000

    created settings for static files.

--- ProjectOnlineOrder/settings.py
+++ ProjectOnlineOrder/settings.py
@@ -132,3 +132,28 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 LOGIN_URL = 'accounts/login'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# enables static files
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "static")
+    ]
+
+
+# AWS_S3_OBJECT_PARAMETERS={
+#     'Expires': 'Thu, 31 Dec 2099, 20:00:00 GMT',
+#     'CacheControl': 'max-age=946800'
+# }
+
+# AWS_STORAGE_BUCKET_NAME="project-4-online-orders"
+# AWS_S3_REGION_NAME="ap-southeast-1"
+# AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
+# AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
+# AWS_S3_CUSTOM_DOMAIN="{}.s3.amazonaws.com".format(AWS_STORAGE_BUCKET_NAME)
+
+# STATICFILES_STORAGE="storages.backends.s3boto3.S3BotoStorage"
+
+
+
+
--- ProjectOnlineOrder/urls.py
+++ ProjectOnlineOrder/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import index, register, login, profile, logout
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('accounts/', include('accounts.urls'))
-]
+    path('accounts/', include('accounts.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
