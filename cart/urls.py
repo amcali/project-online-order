f9eb171d711536cc88cdb992360806ae2bda4a36
Ubuntu <ubuntu@ip-172-31-39-221.ec2.internal>
2019-11-18 05:34:28 +0000

    linked checkout page to cart page.

--- cart/urls.py
+++ cart/urls.py
@@ -2,10 +2,9 @@
 from django.urls import path, include
 from .views import add_to_cart, view_cart, clear_from_cart, reduce_from_cart
 
-
 urlpatterns = [
-    path('add_to_cart/<product_id>', add_to_cart, name='add_to_cart'),
     path('view', view_cart, name='view_cart'),
+    path('add_to_cart/<product_id>', add_to_cart, name='add_to_cart'),    
     path('reduce_from_cart/<product_id>', reduce_from_cart, name="reduce_from_cart"),
     path('clear_from_cart/<cart_item_id>', clear_from_cart, name='clear_from_cart')
 ]
